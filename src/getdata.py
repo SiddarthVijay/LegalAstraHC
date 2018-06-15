e481c73351982f9eefee410d4115b067db50e748
Siddarth Vijay <vsiddarthvijay@gmail.com>
2018-06-15 13:01:21 +0530

    Automated the To date to be the current system date

--- src/getdata.py
+++ src/getdata.py
@@ -1,9 +1,15 @@
 # Import Libraries
 from bs4 import BeautifulSoup
 import requests
+import datetime
+
+# Getting todays date from the system
+today_date = datetime.date.today()
+today_date = today_date.strftime("%d-%m-%Y")
+
 
 bombayhc_URL = "http://www.bombayhighcourt.nic.in/ordqryrepact_action.php"
-payload = {'pageno': '1', 'frmaction': '', 'm_sideflg': 'C', 'actcode': '0', 'frmdate': '01-01-2018', 'todate': '14-06-2018', 'submit1': 'Submit'}
+payload = {'pageno': '1', 'frmaction': '', 'm_sideflg': 'C', 'actcode': '0', 'frmdate': '01-01-2018', 'todate': today_date, 'submit1': 'Submit'}
 
 bombayhc_page = requests.post(bombayhc_URL, payload)
 
@@ -13,7 +19,7 @@ bombayhc_content = BeautifulSoup(bombayhc_page.content, 'html.parser')
 # Finding the judgements table
 data_table = bombayhc_content.find_all('table')[4].find_all('td', {'align': ['center']})
 
-with open("../outputs/table_data_long.txt", "w") as text_file:
+with open("../outputs/table_data.txt", "w") as text_file:
     for data_table_text in data_table:
         # Writing text into file for later use
         print(data_table_text.get_text(), file=text_file)
